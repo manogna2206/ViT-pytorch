48c6516811da5420083132997186b64944625cd4
Manogna <manogna63@gmail.com>
2021-06-17 21:33:09 +0530

    Added setup.sh to install requirements

---
+++ setup.sh
@@ -0,0 +1,10 @@
+mkdir checkpoints
+mkdir checkpoints/pretrained_ckpts
+cd checkpoints/pretrained_ckpts/
+#wget https://storage.googleapis.com/vit_models/imagenet21k/ViT-B_16.npz
+cd ../..
+mkdir checkpoints/output_ckpts
+pip install numpy tqdm torch torchvision scipy pillow matplotlib tabulate ml-collections tensorboard tensorflow
+cd cdfsl_dataset
+python3 setup.py install
+cd ..
